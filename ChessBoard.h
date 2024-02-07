a1723e509b67581ae349a2cad6e3e7f402180fc7
Yoshi-tsuu <107516962+Yoshi-tsuu@users.noreply.github.com>
2024-02-07 14:10:35 +0100

    Update ChessBoard.h

--- ChessBoard.h
+++ ChessBoard.h
@@ -4,20 +4,29 @@
 
 using namespace std;
 
-class Chessboard {
-public:
-    vector<vector<ChessPiece*>> board;
-    bool playerTurn;
-
-    Chessboard();
-    void initializeBoard();
-    void initializeWhitePieces();
-    void initializeBlackPieces();
-    void displayBoard();
-    bool isValidMove(int fromRow, int fromCol, int toRow, int toCol);
-    void makeMove(int fromRow, int fromCol, int toRow, int toCol);
-    bool isCheckmate(bool isWhiteKingInCheck);
-    bool isKingInCheck(int kingRow, int kingCol) const;
-    ~Chessboard();
+struct Coord {
+    Coord(float x, float y) : row(x), col(y) {};
+    float row;
+    float col;
 };
 
+class Chessboard {
+public:
+    vector<vector<ChessPiece*>> board; // tablica zawierajaca plansze
+    vector<Coord> coords; // tablica zawierajaca mozliwe ruchy
+    bool playerTurn;
+    bool endGame = false;
+    Chessboard(); // konstruktor
+    void initializeBoard(); // inicjalizacja
+    void initializeWhitePieces(); // inicjalizcja białych pionków
+    void initializeBlackPieces(); // inicjalizcja czarnych pionków
+    void displayBoard(); // wyświetlanie planszy oraz aktualnych pionków
+    bool isValidMove(int fromRow, int fromCol, int toRow, int toCol); // sprawdzanie ruchu dla konkretnej figury
+    bool select(int fromRow, int fromCol); // podświetlenie wybranego pionka
+    void showMoves(int fromRow, int fromCol); // generowanie możliwych ruchów dla figur
+    void displayMoves(); // wiświtlanie możliwych ruchów figur
+    void makeMove(int fromRow, int fromCol, int toRow, int toCol); // zmiana pozycji pionka
+    void clear(); // czysczenie zaznaczenia
+    bool isKingInCheck(); // sprawdzanie czy któryś z królów jest w szachu
+    ~Chessboard(); // destruktor
+};
