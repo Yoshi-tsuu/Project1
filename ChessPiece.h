11df6d02c5f105035e876cab7160fa31ea8553e3
Yoshi-tsuu <107516962+Yoshi-tsuu@users.noreply.github.com>
2024-02-07 14:09:40 +0100

    Update ChessPiece.h

--- ChessPiece.h
+++ ChessPiece.h
@@ -5,57 +5,52 @@
 
 using namespace std;
 
-class ChessPiece 
-{
+class ChessPiece {
 public:
     virtual char getSymbol() = 0;
     char getColor();
     virtual bool isValidMove(int fromRow, int fromCol, int toRow, int toCol, const vector<vector<ChessPiece*>>& board, char playerSymbol) = 0;
     char color;
+    bool marked = false;
+    bool firstMove = true;
 };
 
-class King : public ChessPiece 
-{
+class King : public ChessPiece {
 public:
     King(char a);
     char getSymbol();
     bool isValidMove(int fromRow, int fromCol, int toRow, int toCol, const vector<vector<ChessPiece*>>& board, char playerSymbol);
 };
 
-class Queen : public ChessPiece 
-{
+class Queen : public ChessPiece {
 public:
     Queen(char a);
     char getSymbol();
     bool isValidMove(int fromRow, int fromCol, int toRow, int toCol, const vector<vector<ChessPiece*>>& board, char playerSymbol);
 };
 
-class Bishop : public ChessPiece 
-{
+class Bishop : public ChessPiece {
 public:
     Bishop(char a);
     char getSymbol();
     bool isValidMove(int fromRow, int fromCol, int toRow, int toCol, const vector<vector<ChessPiece*>>& board, char playerSymbol);
 };
 
-class Knight : public ChessPiece 
-{
+class Knight : public ChessPiece {
 public:
     Knight(char a);
     char getSymbol();
     bool isValidMove(int fromRow, int fromCol, int toRow, int toCol, const vector<vector<ChessPiece*>>& board, char playerSymbol);
 };
 
-class Tower : public ChessPiece 
-{
+class Tower : public ChessPiece {
 public:
     Tower(char a);
     char getSymbol();
     bool isValidMove(int fromRow, int fromCol, int toRow, int toCol, const vector<vector<ChessPiece*>>& board, char playerSymbol);
 };
 
-class Pawn : public ChessPiece 
-{
+class Pawn : public ChessPiece {
 public:
     Pawn(char a);
     char getSymbol();
