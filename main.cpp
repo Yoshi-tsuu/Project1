95d210d91ad6d25d962924054555cee1e98efda5
Yoshi-tsuu <107516962+Yoshi-tsuu@users.noreply.github.com>
2024-02-07 14:09:13 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,31 +1,36 @@
-﻿#include "Chessboard.h"
+#include "Chessboard.h"
 #include "ChessPiece.h"
 #include <iostream>
 
 using namespace std;
 
 int main() {
+    system("chcp 1250>>null");
     Chessboard chessboard;
     chessboard.displayBoard();
-
     int fromRow, fromCol, toRow, toCol;
-
     do {
-        cout << (chessboard.playerTurn ? "Biały" : "Czarny") << ", podaj ruch (np. A2 A4): ";
+        cout << (chessboard.playerTurn ? "Biały" : "Czarny") << ", wybierz bierke (np. A2): ";
         char fromColChar, toColChar;
-        cin >> fromColChar >> fromRow >> toColChar >> toRow;
-
-
+        chessboard.isKingInCheck();
+        cin >> fromColChar >> fromRow;
         fromCol = int(fromColChar) - 65;
-        toCol = int(toColChar) - 65;
-
-
-        chessboard.makeMove(8 - fromRow, fromCol, 8 - toRow, toCol);
-        chessboard.displayBoard();
-
-    } while (!chessboard.isCheckmate(chessboard.playerTurn));
-
+        if(chessboard.select(8 - fromRow, fromCol)) {
+            chessboard.showMoves(8 - fromRow, fromCol);
+            cout << "Możliwe ruchy: " << endl;
+            chessboard.displayMoves();
+            cout << "Podaj ruch (np. A3): ";
+            cin >> toColChar >> toRow;
+            toCol = int(toColChar) - 65;
+            chessboard.makeMove(8 - fromRow, fromCol, 8 - toRow, toCol);
+            chessboard.clear();
+            chessboard.displayBoard();
+        }
+        else {
+            cout << "Błędny wybór \n";
+        }
+    }
+    while (chessboard.endGame == false);
     cout << (chessboard.playerTurn ? "Czarny" : "Biały") << " wygrywa!" << endl;
-
     return 0;
 }
