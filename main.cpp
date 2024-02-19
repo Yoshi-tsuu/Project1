649fa90d98ba1a6ffdac38e4fc97f50d965bf099
Yoshi-tsuu <107516962+Yoshi-tsuu@users.noreply.github.com>
2024-02-19 16:21:14 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,29 +8,35 @@ int main() {
     system("chcp 1250>>null");
     Chessboard chessboard;
     chessboard.displayBoard();
-    int fromRow, fromCol, toRow, toCol;
+    int fromRow, fromCol{}, toRow, toCol;
     do {
-        cout << (chessboard.playerTurn ? "Biały" : "Czarny") << ", wybierz bierke (np. A2): ";
+        chessboard.startTurn();
         char fromColChar, toColChar;
         chessboard.isKingInCheck();
         cin >> fromColChar >> fromRow;
-        fromCol = int(fromColChar) - 65;
+        if (fromColChar > 64 and fromColChar < 73)
+            fromCol = int(fromColChar) - 65;
+        else if (fromColChar > 96 and fromColChar < 105)
+            fromCol = int(fromColChar) - 97;
         if(chessboard.select(8 - fromRow, fromCol)) {
             chessboard.showMoves(8 - fromRow, fromCol);
-            cout << "Możliwe ruchy: " << endl;
+            chessboard.displayMessage("Możliwe ruchy: \n");
             chessboard.displayMoves();
-            cout << "Podaj ruch (np. A3): ";
+            chessboard.displayMessage("Podaj ruch (np. A3): ");
             cin >> toColChar >> toRow;
-            toCol = int(toColChar) - 65;
+            toCol = 0;
+            if (toColChar > 64 and toColChar < 73)
+                toCol = int(toColChar) - 65;
+            else if (toColChar > 96 and toColChar < 105)
+                toCol = int(toColChar) - 97;
             chessboard.makeMove(8 - fromRow, fromCol, 8 - toRow, toCol);
             chessboard.clear();
             chessboard.displayBoard();
         }
         else {
-            cout << "Błędny wybór \n";
+            chessboard.displayMessage("Błędny wybór \n");
         }
-    }
-    while (chessboard.endGame == false);
-    cout << (chessboard.playerTurn ? "Czarny" : "Biały") << " wygrywa!" << endl;
+    } while (chessboard.endGame == false);
+    chessboard.gameEnd();
     return 0;
 }
